9c115c86680f32b6b7a5191924db05e685ce607b
Michael Froelich <MichaelFroelich@users.noreply.github.com>
2016-03-02 14:27:08 +1100

    Licensed

--- ReactivePage.cs
+++ ReactivePage.cs
@@ -1,3 +1,13 @@
+/*
+				GNU GENERAL PUBLIC LICENSE
+		                   Version 3, 29 June 2007
+	 Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+	 Everyone is permitted to copy and distribute verbatim copies
+	 of this license document, but changing it is not allowed.
+	 
+	 	Author: Michael J. Froelich
+ */
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using React;
